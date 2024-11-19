9e053810ec4d69e8ecc151e92a1ff6de68d99cfd
ramakrishnabhavana <rbhavanacm2a@gmail.com>
2024-11-19 09:30:57 +0530

    Add files via upload

---
+++ src/main/java/com/example/to_do_listapp/OnDialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.example.to_do_listapp;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+    void onDialogClass(DialogInterface dialogInterface);
+}
